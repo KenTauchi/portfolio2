e3cb32b2f7ea1a37b8c127e338ea2d62ad9ba389
Ken Tauchi <kentauchica@gmail.com>
2025-02-13 19:27:45 -0800

    fix(meta): fix metatag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,13 +7,19 @@ export default function Document() {
         <link rel="icon" type="image/svg+xml" href="/ken_logo.svg" />
         <link rel="alternate icon" href="/favicon.ico" />
 
+        {/* Primary Meta Tags */}
+        <meta
+          name="description"
+          content="Ken Tauchi - Software Developer portfolio showcasing frontend development expertise and scalable application development."
+        />
+
         {/* Open Graph / LinkedIn */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://kentauchi.com/" />
         <meta property="og:title" content="Ken Tauchi | Software Developer" />
         <meta
           property="og:description"
-          content="Experienced Software Developer specializing in fontend development, and building scalable applications."
+          content="Ken Tauchi - Software Developer portfolio showcasing frontend development expertise and scalable application development."
         />
         <meta property="og:image" content="https://kentauchi.com/ken_logo.svg" />
 
@@ -23,7 +29,7 @@ export default function Document() {
         <meta name="twitter:title" content="Ken Tauchi | Software Developer" />
         <meta
           name="twitter:description"
-          content="Experienced Software Developer specializing in fontend development, and building scalable applications."
+          content="Ken Tauchi - Software Developer portfolio showcasing frontend development expertise and scalable application development."
         />
         <meta name="twitter:image" content="https://kentauchi.com/ken_logo.svg" />
       </Head>
