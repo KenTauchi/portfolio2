4ce6452324ebdb7186dc9e0264190eef0222ec16
Ken Tauchi <kentauchica@gmail.com>
2025-02-13 19:24:44 -0800

    fix(meta): fix metatag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,20 +10,20 @@ export default function Document() {
         {/* Open Graph / LinkedIn */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://kentauchi.com/" />
-        <meta property="og:title" content="Ken Tauchi | Software Engineer" />
+        <meta property="og:title" content="Ken Tauchi | Software Developer" />
         <meta
           property="og:description"
-          content="Experienced Software Engineer specializing in fontend development, and building scalable applications."
+          content="Experienced Software Developer specializing in fontend development, and building scalable applications."
         />
         <meta property="og:image" content="https://kentauchi.com/ken_logo.svg" />
 
         {/* Twitter Card */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:url" content="https://kentauchi.com/" />
-        <meta name="twitter:title" content="Ken Tauchi | Software Engineer" />
+        <meta name="twitter:title" content="Ken Tauchi | Software Developer" />
         <meta
           name="twitter:description"
-          content="Experienced Software Engineer specializing in fontend development, and building scalable applications."
+          content="Experienced Software Developer specializing in fontend development, and building scalable applications."
         />
         <meta name="twitter:image" content="https://kentauchi.com/ken_logo.svg" />
       </Head>
