f47ffc4a3b8eec23de63ab2fc3346a4d0762ee9b
Ken Tauchi <kentauchica@gmail.com>
2025-02-13 19:23:08 -0800

    fix(favicon): update meta and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,30 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/ken_logo.svg" />
+        <link rel="alternate icon" href="/favicon.ico" />
+
+        {/* Open Graph / LinkedIn */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://kentauchi.com/" />
+        <meta property="og:title" content="Ken Tauchi | Software Engineer" />
+        <meta
+          property="og:description"
+          content="Experienced Software Engineer specializing in fontend development, and building scalable applications."
+        />
+        <meta property="og:image" content="https://kentauchi.com/ken_logo.svg" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content="https://kentauchi.com/" />
+        <meta name="twitter:title" content="Ken Tauchi | Software Engineer" />
+        <meta
+          name="twitter:description"
+          content="Experienced Software Engineer specializing in fontend development, and building scalable applications."
+        />
+        <meta name="twitter:image" content="https://kentauchi.com/ken_logo.svg" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
